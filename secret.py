39a339eaba323d37a40922b1c97ef3028d33570e
ashujha301 <55935137+ashujha301@users.noreply.github.com>
2021-11-11 20:33:10 +0530

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,3 @@
+senderemail='ashujha301@gmail.com'
+epwd='cocacola@pile'
+to='ftvwhuwlkxicnvbgoi@pp7rvv.com'
---
+++ secrets.py
@@ -0,0 +1,3 @@
+senderemail='ashujha301@gmail.com'
+epwd='cocacola@pile'
+to='vspwdbbdyndfldryew@mrvpm.net'
